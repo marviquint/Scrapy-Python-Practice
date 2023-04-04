f99d31e6c00016ed4afb75c04d16e0ba623e19e2
neilhavoc <sarioneiljohm@gmail.com>
2023-04-04 21:00:52 +0800

    commit

--- quotetutorial/quotetutorial/spiders/qoutes_spider.py
+++ quotetutorial/quotetutorial/spiders/qoutes_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from ..items import QuotetutorialItem
 class QouteSpider(scrapy.Spider):
     name = "qoutes"
     start_urls = [
@@ -7,15 +7,24 @@ class QouteSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        # title = response.css('title').extract()
-        # yield {'titletext': title}
-        #all_div_qoutes = response.css('div.qoute')[0]
-        tile = response.css('span.text::text')[0].extract()
+        items = QuotetutorialItem()
+
+        title = response.css('span.text::text')[0].extract()
         author = response.css('.author::text')[0].extract()
         tag = response.css('.tag::text')[0].extract()
-        yield {
-            'title' : tile,
-            'author' : author,
-            'tag' : tag
-        }
 
+        items['title'] = title
+        items['author'] = author
+        items['tag'] = tag
+
+        yield items
+
+        # yield {
+        #     'title' : tile,
+        #     'author' : author,
+        #     'tag' : tag
+        # }
+
+ # title = response.css('title').extract()
+        # yield {'titletext': title}
+        #all_div_qoutes = response.css('div.qoute')[0]
