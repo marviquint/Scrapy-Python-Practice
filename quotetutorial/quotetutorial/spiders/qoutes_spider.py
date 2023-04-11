922e90f4a024ea4210bd1e2bb71dd6167d28cc38
neilhavoc <sarioneiljohm@gmail.com>
2023-04-11 17:49:37 +0800

    SCRAPY SCRAPING

--- quotetutorial/quotetutorial/spiders/qoutes_spider.py
+++ quotetutorial/quotetutorial/spiders/qoutes_spider.py
@@ -1,30 +1,43 @@
 import scrapy
 from ..items import QuotetutorialItem
+
 class QouteSpider(scrapy.Spider):
     name = "quotes"
+    page_number = 2
     start_urls = [
-        'https://quotes.toscrape.com/'
+        'https://quotes.toscrape.com/page/1/'
     ]
 
     def parse(self, response):
         items = QuotetutorialItem()
+        all_div_quotes = response.css('div.quote')
 
-        title = response.css('span.text::text')[0].extract()
-        author = response.css('.author::text')[0].extract()
-        tag = response.css('.tag::text')[0].extract()
+        for quotes in all_div_quotes:
+            
+            title = quotes.css('span.text::text').extract()
+            author = quotes.css('.author::text').extract()
+            tag = quotes.css('.tag::text').extract()
 
-        items['title'] = title
-        items['author'] = author
-        items['tag'] = tag
+            items['title'] = title
+            items['author'] = author
+            items['tag'] = tag
 
-        yield items
+            yield items
+        
+        # next_page = response.css('li.next a::attr(href)').get()
+        next_page = 'https://quotes.toscrape.com/page/'+ str(QouteSpider.page_number) + '/'
+        print(next_page)
 
-        # yield {
-        #     'title' : tile,
-        #     'author' : author,
-        #     'tag' : tag
-        # }
+        if QouteSpider.page_number < 11:
+            QouteSpider.page_number += 1
+            yield response.follow(next_page, callback=self.parse)
+    
 
- # title = response.css('title').extract()
-        # yield {'titletext': title}
-        #all_div_qoutes = response.css('div.qoute')[0]
+        # if next is not None:
+        #     yield response.follow(next_page, callback = self.parse)
+
+        # # Export the data to a JSON file
+        # filename = 'quotes.json'
+        # with open(filename, 'w') as f:
+        #     f.write(items.to_json())
+        # self.log('Saved file %s' % filename)
